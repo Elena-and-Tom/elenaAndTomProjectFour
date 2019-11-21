addde0d8c6b306a285b2363ae74621f8ef8d42b7
Tommy <tomstuart123@hotmail.co.uk>
2019-11-21 13:55:52 -0500

    api issue solved... lets get to appending :)

--- index.js
+++ index.js
@@ -9,23 +9,21 @@ wineApp.topSection = document.querySelector(".topSection");
 // wineApp.userChoice = [];
 
 // functions for on clicks
-wineApp.events = function() {
+wineApp.events = function () {
   $("button").on("click", e => {
     let userChoice = "";
 
     e.preventDefault();
     userChoice = $("select[name=foodChoice]").val();
-    const data = wineApp.getData(userChoice);
+    wineApp.getData(userChoice);
     wineApp.topSection.className += "topSectionHide";
     wineApp.mainContainer.innerHTML = "";
-    wineApp.displayTypes(userChoice, data);
   });
 };
 
 // functions for api calls
-wineApp.getData = function(foodChoice) {
+wineApp.getData = function (foodChoice) {
   wineApp.url = "https://api.spoonacular.com/food/wine/pairing?";
-  let apiData = [];
 
   $.ajax({
     url: wineApp.url,
@@ -37,30 +35,45 @@ wineApp.getData = function(foodChoice) {
       apiKey: "98162948a5504d3181cf99fb1f42ea76"
     }
   })
-    .then(function(result) {
+    .then(function (result) {
       console.log(result);
-      apiData.push(result.pairedWines);
-      apiData.push(result.pairingText);
-      console.log(apiData);
+      wineApp.cleanData(result, foodChoice);
+
     })
-    .fail(function() {
+    .fail(function () {
       console.log("fail");
     });
 
-  return apiData;
+
 };
 
+// wineApp.pairedWines;
+
+wineApp.cleanData = function (apiData, userChoice) {
+  let totalData = [];
+  let pairedArray = apiData.pairedWines;
+  let pairedDescription = apiData.pairingText;
+  totalData.push(pairedArray)
+  totalData.push(pairedDescription)
+  wineApp.displayTypes(totalData, userChoice);
+}
+
+
+
 // functions for activity
-wineApp.displayTypes = function(userInput, apiData) {
+wineApp.displayTypes = function (apiData, userInput) {
   // grab the data from the API
-  console.log(userInput, apiData);
+
   console.log(apiData[0]);
+  console.log(apiData[1]);
+
+
   // const wines = apiData[0][0];
   // console.log(wines);
   // wines.join(",");
 
   $(".mainContainer").append(
-    `<div class="pageTwoTop"><p>You chose ${userInput}. Good choice!</p><p>Top picks for you:${wines}</p></div>`
+    `<div class="pageTwoTop"><p>You chose ${userInput}. Good choice!</p><p>Top picks for you:${apiData[0]}</p></div>`
   );
 };
 
@@ -68,6 +81,6 @@ wineApp.init = () => {
   wineApp.events();
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   wineApp.init();
 });
