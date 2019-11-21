97c9d99e4b684c19670d481e9b5b469f858fda58
elenaserova <elein.serova@gmail.com>
2019-11-21 14:51:56 -0500

    wine types appended to the next screen and start over button functionality added

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ wineApp.topSection = document.querySelector(".topSection");
 // wineApp.userChoice = [];
 
 // functions for on clicks
-wineApp.events = function () {
-  $("button").on("click", e => {
+wineApp.events = function() {
+  $("#getResults").on("click", e => {
     let userChoice = "";
 
     e.preventDefault();
@@ -19,10 +19,38 @@ wineApp.events = function () {
     wineApp.topSection.className += "topSectionHide";
     wineApp.mainContainer.innerHTML = "";
   });
+
+  $(".mainContainer").on("click", "#startOver", e => {
+    e.preventDefault();
+    wineApp.startOver();
+  });
+};
+
+wineApp.startOver = function() {
+  $(".mainContainer").html("").append(`<div class="topSection">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet ipsam,
+          id repudiandae facere ut magni! Facere, expedita? Totam, fuga iure.
+          Minus officiis deleniti earum rerum quo dicta unde sit sapiente.
+        </p>
+      </div>
+      <form class="userSelection" id="userSelection">
+        <label for="foodChoice">Please select the food</label>
+        <select name="foodChoice">
+          <option selected="selected">Select the food</option>
+          <option value="red meat">Red meat</option>
+          <option value="chicken">White meat </option>
+          <option value="pizza">Pizza</option>
+          <option value="risotto">Risotto(V)</option>
+          <option value="fish">Fish</option>
+        </select>
+      </form>
+      <button type="submit" id="getResults">Explore suggestions</button>
+    </div>`);
 };
 
 // functions for api calls
-wineApp.getData = function (foodChoice) {
+wineApp.getData = function(foodChoice) {
   wineApp.url = "https://api.spoonacular.com/food/wine/pairing?";
 
   $.ajax({
@@ -35,52 +63,64 @@ wineApp.getData = function (foodChoice) {
       apiKey: "98162948a5504d3181cf99fb1f42ea76"
     }
   })
-    .then(function (result) {
+    .then(function(result) {
       console.log(result);
       wineApp.cleanData(result, foodChoice);
-
     })
-    .fail(function () {
+    .fail(function() {
       console.log("fail");
     });
-
-
 };
 
 // wineApp.pairedWines;
 
-wineApp.cleanData = function (apiData, userChoice) {
+wineApp.cleanData = function(apiData, userChoice) {
   let totalData = [];
   let pairedArray = apiData.pairedWines;
   let pairedDescription = apiData.pairingText;
-  totalData.push(pairedArray)
-  totalData.push(pairedDescription)
+  totalData.push(pairedArray);
+  totalData.push(pairedDescription);
   wineApp.displayTypes(totalData, userChoice);
-}
-
-
+};
 
 // functions for activity
-wineApp.displayTypes = function (apiData, userInput) {
+wineApp.displayTypes = function(apiData, userInput) {
   // grab the data from the API
 
   console.log(apiData[0]);
   console.log(apiData[1]);
 
-
   // const wines = apiData[0][0];
   // console.log(wines);
-  // wines.join(",");
+  let pairedWines;
+  pairedWines = apiData[0].join(", ");
+  console.log(pairedWines);
 
   $(".mainContainer").append(
-    `<div class="pageTwoTop"><p>You chose ${userInput}. Good choice!</p><p>Top picks for you:${apiData[0]}</p></div>`
-  );
+    `<div class="pageTwoTop"><p>You chose ${userInput}. Good choice!</p><p>Top picks for you:<span class="pairedWines">${pairedWines}</span></p><p>${apiData[1]}</p></div>`
+  ).append(`<div><form class="wineSelection" id="wineSelection">
+        <legend> I'd love to get recommendations for:</legend>
+        
+        <input type="radio" name="wineChoice1" value=${apiData[0][0]}>
+        <label for="wineChoice1">${apiData[0][0]}</label>
+        
+        <input type="radio" name="wineChoice2" value=${apiData[0][1]}>
+        <label for="wineChoice2">${apiData[0][1]}</label>
+        
+        <input type="radio" name="wineChoice3" value=${apiData[0][2]}>
+        <label for="wineChoice3">${apiData[0][2]}</label>
+        
+          
+      </form>
+      <button type="submit" id="getResultsTwo">Get recommedations</button>
+      <button type="submit" id="startOver">Start over</button>
+      </div>`);
 };
 
 wineApp.init = () => {
   wineApp.events();
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
   wineApp.init();
 });
